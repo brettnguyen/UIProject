1453be3cb078cb74cb9e313030517d8671aedcfc
BrettNguyen <brettnguyen7@gmail.com>
2022-06-30 17:10:59 -0400

    navbar links collapse navbar

--- client/src/components/pages/Navbar.js
+++ client/src/components/pages/Navbar.js
@@ -2,28 +2,35 @@ import React from 'react';
 import {  Link } from "react-router-dom";
 
 const Navbar = () => {
-  
+  function myFunction() {
+    console.log("handleCollapse");
+    var nav = document.getElementById("navbarNav");
+    var btn = document.getElementById("navbarBtn");
+    nav.classList.remove("show");
+    btn.classList.add("collapsed");
+}
+
     return (
       <div>
         <nav className="navbar text-center">
           <div className="container">
            
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <input type="text" placeholder="Search.."></input>
             <div className="collapse navbar-collapse " id="navbarNav">
               <ul className="navbar-nav ">
-              <li  >
-                <Link to="/" >Login</Link>
+              <li>
+                <Link to="/" onClick={myFunction} >Login</Link>
                 </li>
                 
-                <li >
-                <Link to="/Register">Register</Link>
+                <li>
+                <Link to="/Register" onClick={myFunction}>Register</Link>
                 </li>
 
-                <li >
-                <Link to="/Profile" >Profile</Link>
+                <li onClick={myFunction} >
+                <Link to="/Profile" onClick={myFunction} >Profile</Link>
                 </li>
               </ul>
             </div>
